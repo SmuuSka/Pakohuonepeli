44947d6e19e032eacd10ff8aa03211d884c3d2f8
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-05-21 15:50:42 +0300

    Semmosta

--- Assets/Scripts/CheckAnswerScript.cs
+++ Assets/Scripts/CheckAnswerScript.cs
@@ -8,6 +8,11 @@ public class CheckAnswerScript : MonoBehaviour
 {
     [SerializeField] private Text[] slot = new Text[0];
     [SerializeField] private Text[] answer = new Text[0];
+    [Space]
+    [Space]
+    [SerializeField] private Button nextSlot;
+    [SerializeField] private Button previousSlot;
+    [SerializeField] private Button sendAnswer;
 
     private CursorScrip cursorManager;
 
@@ -23,67 +28,63 @@ public class CheckAnswerScript : MonoBehaviour
         slot[3].text = " 0 ";
         slot[4].text = " 0 ";
 
-        answer[0].text = Random.Range(0, 36).ToString();
-        answer[1].text = Random.Range(0, 36).ToString();
-        answer[2].text = Random.Range(0, 36).ToString();
-        answer[3].text = Random.Range(0, 36).ToString();
-        answer[4].text = Random.Range(0, 36).ToString();
+        answer[0].text = Random.Range(0, 35).ToString();
+        answer[1].text = Random.Range(0, 35).ToString();
+        answer[2].text = Random.Range(0, 35).ToString();
+        answer[3].text = Random.Range(0, 35).ToString();
+        answer[4].text = Random.Range(0, 35).ToString();
 
         slotInt = 0;
+
+        nextSlot.onClick.AddListener(GoNext);
+        previousSlot.onClick.AddListener(GoBack);
+        sendAnswer.onClick.AddListener(CheckAnswerIsPressed);
     }
 
     private void CheckAnswer()
     {
-        slot[slotInt].text = cursorManager.currentValue.ToString();
-
-        switch (slotInt)
+        for (int i = 0; i < answer.Length; i++)
         {
-
-            case 0:
-                if (slot[slotInt].text == answer[slotInt].text)
-                {
-                    slot[slotInt].color = Color.green;
-                    slotInt++;
-                }
-                break;
-
-            case 1:
-                if (slot[slotInt].text == answer[slotInt].text)
-                {
-                    slot[slotInt].color = Color.green;
-                    slotInt++;
-                }
-                break;
-
-            case 2:
-                if (slot[slotInt].text == answer[slotInt].text)
-                {
-                    slot[slotInt].color = Color.green;
-                    slotInt++;
-                }
-                break;
-
-            case 3:
-                if (slot[slotInt].text == answer[slotInt].text)
-                {
-                    slot[slotInt].color = Color.green;
-                    slotInt++;
-                }
-                break;
-
-            case 4:
-                if (slot[slotInt].text == answer[slotInt].text)
-                {
-                    slot[slotInt].color = Color.green;
-                    SceneManager.LoadScene(1);
-                }
-                break;
-
+            if (slot[i].text == answer[i].text)
+            {
+                slot[i].color = Color.green;
+            }
+            else
+            {
+                slot[i].color = Color.red;
+            }
         }
     }
 
     private void Update()
     {
+        LockWheelNumber();
+    }
+
+    public void GoNext()
+    {
+        if (slotInt < 4)
+        {
+
+            slotInt++;
+        }
+        
+    }
+    public void GoBack()
+    {
+        if (slotInt > 0)
+        {
+            slotInt--;
+        }
+    }
+    public void CheckAnswerIsPressed()
+    {
         CheckAnswer();
+    }
+
+    private void LockWheelNumber()
+    {
+        slot[slotInt].text = cursorManager.currentValue.ToString();
+        Debug.Log("Current Slot " + slotInt);
     }
 }
--- Assets/Scripts/CursorScrip.cs
+++ Assets/Scripts/CursorScrip.cs
@@ -20,7 +20,7 @@ public class CursorScrip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Value: " + currentValue);
+        //Debug.Log("Value: " + currentValue);
         Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPos.z = 0f;
         transform.position = mouseWorldPos;
