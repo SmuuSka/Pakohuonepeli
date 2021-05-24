ff3caf5ffc6da06b7a603b901d11097f1f2fe8eb
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-05-24 11:23:57 +0300

    update

--- Assets/Scripts/ObjectHighlight.cs
+++ Assets/Scripts/ObjectHighlight.cs
@@ -6,17 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class ObjectHighlight : MonoBehaviour
 {
-    [SerializeField] Color highlightColor;
-    [SerializeField] Color defaultColor;
+    [SerializeField] private GameObject highlight;
 
     private bool moveToTask;
 
     void Start()
     {
-       
+        highlight.SetActive(false);
     }
     void Update()
     {
 
+    }
+
+    private void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
     }
 }
