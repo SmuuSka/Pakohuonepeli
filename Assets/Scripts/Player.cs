9b8c5e830757a3c1088152250c5cf321d5462380
Japeee <74297488+Japeee@users.noreply.github.com>
2021-05-28 08:49:42 +0300

    Inventoryn lisäys

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform player;
+
+    void Awake()
+    {
+        //Find the player object and set it
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {   
+            player.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject currentInterObj = null;
+
+    public Pickup currentPickupScript = null;
+
+    public Inventory inventory; 
+
+
+    //public GameObject itemButton;
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && currentInterObj) { 
+            if (currentPickupScript.inventory)
+            {
+                currentPickupScript.Additem(currentInterObj);
+                
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+            currentPickupScript = currentInterObj.GetComponent<Pickup>();
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            if (other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }
+    }
+}
