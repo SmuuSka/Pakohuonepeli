bcd723f6c7cd879c92c470973d43bed2f672ff15
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-05-31 10:29:42 +0300

    liikkuu

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Camera playerCamera;
+
+    private float smoothSpeed = 0.125f;
+    private Vector3 offset = new Vector3(0,0,-10);
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
+
---
+++ Assets/Scripts/RobotMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotMoveScript : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    private float horizontalInput;
+    public float forceMultiplier = 200f, forceMultiplierMinus = -200;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerRb.velocity = Vector2.right * horizontalInput * forceMultiplier * Time.deltaTime;
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerRb.velocity = Vector2.left * horizontalInput * forceMultiplierMinus * Time.deltaTime;
+        }
+    }
+}
