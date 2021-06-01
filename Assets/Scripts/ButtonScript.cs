02a43835bad4d1e2cc925f83a8025e646c569313
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-06-01 11:27:06 +0300

    update

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField] private Button buttonObject;
+
+    private void Start()
+    {
+        buttonObject.onClick.AddListener(GoGame);
+    }
+
+    private void GoGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/ObjectHighlight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ObjectHighlight : MonoBehaviour
+{
+    [SerializeField] private GameObject highlight;
+    private bool highlightObject;
+
+    private void Start()
+    {
+        highlight.SetActive(false);
+    }
+}
