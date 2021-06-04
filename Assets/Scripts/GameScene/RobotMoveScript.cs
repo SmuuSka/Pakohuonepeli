ca10365e0e0e9030a972586b721e1add9aecdca0
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-06-04 21:27:37 +0300

    update

--- Assets/Scripts/GameScene/RobotMoveScript.cs
+++ Assets/Scripts/GameScene/RobotMoveScript.cs
@@ -6,6 +6,8 @@ public class RobotMoveScript : MonoBehaviour
 {
     [SerializeField] private Transform playerFlip;
 
+    [SerializeField] private Transform castPoint;
+
     private Rigidbody2D playerRb;
     private float horizontalInput;
     private float forceMultiplier = 200f, forceMultiplierMinus = -200;
@@ -20,7 +22,19 @@ public class RobotMoveScript : MonoBehaviour
 
     private void Update()
     {
+        
         horizontalInput = Input.GetAxisRaw("Horizontal");
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * 3f, Color.red);
+        LayerMask mask = LayerMask.GetMask("Room");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), 3f, 9);
+        
+        if (Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), 3f, mask))
+        {
+            if (hit.distance <= 3f && GameObject.Find("/Huone_Final_1/Oik_ovi").tag == "Door")
+            {
+                GameObject.Find("/Huone_Final_1/Oik_ovi").SetActive(false);
+            }
+        }  
     }
 
     private void FixedUpdate()
@@ -35,5 +49,10 @@ public class RobotMoveScript : MonoBehaviour
             playerFlip.rotation = new Quaternion(0, 180, 0, 0);
             playerRb.velocity = Vector2.left * horizontalInput * forceMultiplierMinus * Time.deltaTime;
         }
+    }
+
+    private void OpenDoor()
+    {
+
     }
 }
