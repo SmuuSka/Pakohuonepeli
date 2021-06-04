567f189dba1bc782b9a481b3319b84e245f2c0e5
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-06-04 09:42:55 +0300

    update

--- Assets/Scripts/MinigameScene/FillArea.cs
+++ Assets/Scripts/MinigameScene/FillArea.cs
@@ -5,24 +5,25 @@ using UnityEngine.UI;
 
 public class FillArea : MonoBehaviour
 {
-    [SerializeField] private Slider fillSlider;
+    [SerializeField] private Image fillSlider;
     [SerializeField] private GameObject referenceValue;
     private float value;
     // Start is called before the first frame update
     void Start()
     {
-        fillSlider.value = 0;
+        fillSlider.fillAmount = 0;
+        
+        
         
         
         //fillSlider.value = value;
-        Debug.Log("Fill Slider Status " + fillSlider.value);
+        //Debug.Log("Fill Slider Status " + fillSlider.value);
     }
 
     // Update is called once per frame
     void Update()
     {
-        value = referenceValue.GetComponent<CursorScrip>().currentValue / 100 * 3.6f;
-        Debug.Log(value);
-        fillSlider.value = value;
+        value = referenceValue.GetComponent<CursorScrip>().currentValue / 100 * 2.7777f;
+        fillSlider.fillAmount = value;
     }
 }
