6e94709dc8a35b5058e739b42a91626f55a6e6e0
Japeee <74297488+Japeee@users.noreply.github.com>
2021-06-17 11:19:27 +0300

    Up

---
+++ Assets/Scripts/SlidePuzzleScripts/Block.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public event System.Action<Block> OnBlockPressed;
+    public event System.Action OnFinishedMoving;
+    public Vector2Int coord;
+
+    public void Init(Vector2Int startingCoord, Texture2D image)
+    {
+        coord = startingCoord;
+
+        GetComponent<MeshRenderer>().material = Resources.Load<Material>("Holo_Mat");
+        GetComponent<MeshRenderer>().material.SetTexture("_mainTex", image); 
+        
+    }
+
+    public void MoveToPosition(Vector2 target, float duration)
+    {
+        StartCoroutine(AnimateMove(target, duration));
+    }
+
+    void OnMouseDown()
+    {
+     if (OnBlockPressed != null)
+        {
+            OnBlockPressed(this);
+        }   
+    }
+
+    IEnumerator AnimateMove(Vector2 target, float duration)
+    {
+        Vector2 initialPos = transform.position;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / duration;
+            transform.position = Vector2.Lerp(initialPos, target, percent);
+            yield return null;
+        }
+
+        if (OnFinishedMoving != null)
+        {
+            OnFinishedMoving();
+        }
+    }
+}
---
+++ Assets/Scripts/SlidePuzzleScripts/Puzzle.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public Texture2D image;
+    public int blocksPerLine = 4;
+    public int shuffleLength = 10;
+    public float defaultMoveDuration = .2f;
+    public float shuffleMoveDuration = .1f;
+
+    enum PuzzleState { Solved, Shuffling, InPlay };
+    PuzzleState state;
+
+    Block emptyBlock;
+    Block[,] blocks;
+    Queue<Block> inputs;
+    bool blockIsMoving;
+    int shuffleMoveRemaining;
+    Vector2Int prevShuffleOffset;
+
+    void Start()
+    {
+        CreatePuzzle();
+        if(state == PuzzleState.Solved)
+        {
+            StartShuffle();
+        }
+    }
+    void Update()
+    {
+       /* if (state == PuzzleState.Solved && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartShuffle();
+        }*/
+    }
+
+    void CreatePuzzle()
+    {
+        blocks = new Block[blocksPerLine, blocksPerLine];
+        Texture2D[,] imageSlices = ImageSlicer.GetSlices(image, blocksPerLine);
+        for (int y = 0; y < blocksPerLine; y++)
+        {
+            for (int x = 0; x < blocksPerLine; x++)
+            {
+                GameObject blockObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                blockObject.transform.position = -Vector2.one * (blocksPerLine - 1) * .5f + new Vector2(x, y);
+                blockObject.transform.parent = transform;
+
+                Block block = blockObject.AddComponent<Block>();
+                block.OnBlockPressed += PlayerMoveBlockInput;
+                block.OnFinishedMoving += OnBlockFinishedMoving;
+                block.Init(new Vector2Int(x, y), imageSlices[x, y]);
+                blocks[x, y] = block;
+
+                if (y == 0 && x == blocksPerLine - 1)
+                {
+                    blockObject.SetActive(false);
+                    emptyBlock = block;
+                }
+            }
+        }
+        Camera.main.orthographicSize = blocksPerLine * .65f;
+        inputs = new Queue<Block>();
+    }
+    void PlayerMoveBlockInput(Block blockToMove)
+    {
+        if(state == PuzzleState.InPlay)
+        {
+            inputs.Enqueue(blockToMove);
+            MakeNextPlayerMove();
+        }
+
+    }
+
+    void MakeNextPlayerMove()
+    {
+        while (inputs.Count > 0 && !blockIsMoving)
+        {
+
+            MoveBlock(inputs.Dequeue(), defaultMoveDuration);
+        }
+    }
+    void MoveBlock(Block blockToMove, float duration)
+    {
+        if ((blockToMove.coord - emptyBlock.coord).sqrMagnitude == 1)
+        {
+            blocks[blockToMove.coord.x, blockToMove.coord.y] = emptyBlock;
+            blocks[emptyBlock.coord.x, emptyBlock.coord.y] = blockToMove;
+
+            Vector2Int targetCoord = emptyBlock.coord;
+            emptyBlock.coord = blockToMove.coord;
+            blockToMove.coord = targetCoord;
+
+            Vector2 targetPosition = emptyBlock.transform.position;
+            emptyBlock.transform.position = blockToMove.transform.position;
+            blockToMove.MoveToPosition(targetPosition, duration);
+            blockIsMoving = true;
+        }
+    }
+
+    void OnBlockFinishedMoving()
+    {
+        blockIsMoving = false;
+        if (state == PuzzleState.InPlay)
+        {
+            MakeNextPlayerMove();
+        }
+        else if (state == PuzzleState.Shuffling)
+        {
+            if (shuffleMoveRemaining > 0)
+            {
+                MakeNextShuffleMove();
+            }
+            else
+            {
+                state = PuzzleState.InPlay;
+            }
+        }
+    }
+
+    void StartShuffle()
+    {
+        state = PuzzleState.Shuffling;
+        shuffleMoveRemaining = shuffleLength;
+        MakeNextShuffleMove();
+    }
+
+    void MakeNextShuffleMove()
+    {
+        Vector2Int[] offsets = { new Vector2Int(1, 0), new Vector2Int(-1, 0), new Vector2Int(0, 1), new Vector2Int(0, -1) };
+        int randomIndex = Random.Range(0, offsets.Length);
+
+        for (int i = 0; i < offsets.Length; i++)
+        {
+            Vector2Int offset = offsets[(randomIndex + i) % offsets.Length];
+            if (offset != prevShuffleOffset * -1)
+            {
+                Vector2Int moveBlockCoord = emptyBlock.coord + offset;
+
+                if (moveBlockCoord.x >= 0 && moveBlockCoord.x < blocksPerLine && moveBlockCoord.y >= 0 && moveBlockCoord.y < blocksPerLine)
+                {
+                    MoveBlock(blocks[moveBlockCoord.x, moveBlockCoord.y], shuffleMoveDuration);
+                    shuffleMoveRemaining--;
+                    prevShuffleOffset = offset;
+                    break;
+                }
+            }
+        }
+    }
+}
