b95844704a3085d66ecaeadadcaef7090a8ba4fd
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-06-18 10:50:45 +0300

    up

--- Assets/UI/UIScripts/Dropzone.cs
+++ Assets/UI/UIScripts/Dropzone.cs
@@ -8,11 +8,21 @@ public class Dropzone : MonoBehaviour
     Transform center;
     GameObject ruuvvari;
     public Slot funct;
+
+    [SerializeField]
+    GameObject patteri;
+    public Slot unct;
+
     private void Start()
     {
         ruuvvari = GameObject.Find("Scruuvvari");
         ruuvvari.SetActive(false);
         funct = GameObject.Find("Slot").GetComponent<Slot>();
+
+        patteri = GameObject.Find("Patteri");
+        patteri.SetActive(false);
+        unct = GameObject.Find("Slot").GetComponent<Slot>();
+
     }
     private void OnMouseDown()
     {
@@ -23,6 +33,13 @@ public class Dropzone : MonoBehaviour
             ruuvvari.SetActive(true);
             Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
             funct.DropItem();
+        }
+
+        if (this.gameObject.tag == "DropZoneBattery" && GameObject.Find("Hand").GetComponent<Inventory>().isFull[0] == true && GameObject.Find("Canvas").GetComponentInChildren<UICursorScript>().cursorActive == true)
+        {
+            patteri.SetActive(true);
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+            unct.DropItem();
         }
     }
 }
