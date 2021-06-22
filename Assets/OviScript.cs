412ef43754ffb4c5ec097bf87d533e6769b0e8b0
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-06-22 10:40:42 +0300

    up

---
+++ Assets/OviScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OviScript : MonoBehaviour
+{
+    public Animator doorAnim;
+    public bool isAnimating;
+
+    void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (PlayerData.laserTaskDone == true)
+        {
+            doorAnim.SetBool("isAnimating", true);
+        }
+        else if (PlayerData.laserTaskDone == false)
+        {
+            doorAnim.SetBool("isAnimating", false);
+        }
+    }
+}
