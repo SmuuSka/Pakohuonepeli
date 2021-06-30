edb7b2828b56d228350c588d8917e336b1717fee
Japeee <74297488+Japeee@users.noreply.github.com>
2021-06-30 11:52:33 +0300

    Up

---
+++ Assets/UI/UIScripts/Lappu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lappu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lappu;
+
+    private void Update()
+    {
+        lappu = GameObject.Find("LappuVastaukset");
+    }
+    public void OnMouseDown()
+    {
+        lappu.SetActive(true);
+    }
+}
---
+++ Assets/UI/UIScripts/Vastaukset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vastaukset : MonoBehaviour
+{
+    public GameObject answers;
+
+    public void Start()
+    {
+        answers = GameObject.Find("LappuVastaukset");
+        answers.SetActive(false);
+    }
+
+}
