c62065f0f84806e5fd674cfc2918ac98ab9a9fec
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-06-30 10:54:13 +0300

    Pause menu up

---
+++ Assets/InGameUIDisabled.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIDisabled : MonoBehaviour
+{
+
+    static GameObject inGameUI;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
