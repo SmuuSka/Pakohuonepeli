4e25c3614fc7a6a2b233fe3cd59bb4d68c03a0ad
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-07-08 14:39:43 +0300

    SAATANA

--- Assets/Scripts/GameScene/Propeller.cs
+++ Assets/Scripts/GameScene/Propeller.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class Propeller : MonoBehaviour
 {
-    private int speed = 8;
-    private int multiplier = 100;
-    
-    private void Update()
-    {
-        
-    }
-    private void FixedUpdate()
+    public int speed = 8;
+    public int multiplier = 100;  
+ 
+    public void FixedUpdate()
     {
         transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + speed * multiplier * Time.deltaTime, transform.rotation.z));
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other)
         {
+            GameObject.Find("/Explosion/ExplosionEffect").GetComponent<ParticleSystem>().Play();
+            GameObject.Find("Explosion").GetComponent<AudioSource>().Play();
             Destroy(this.gameObject);
             GameObject.Find("/Ventti (1)/Dropzone").GetComponent<BoxCollider2D>().enabled = false;
         }
