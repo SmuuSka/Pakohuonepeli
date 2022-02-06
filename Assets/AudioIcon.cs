5f1a152debb55a4ba1112f614734541a7536e1f8
Samu <65758544+SmuuSka@users.noreply.github.com>
2022-02-06 14:32:14 +0200

    some_fix

--- Assets/AudioIcon.cs
+++ Assets/AudioIcon.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AudioIcon : MonoBehaviour
 {
     [SerializeField] private Image[] audioIcon = new Image[0];
     [SerializeField] private Slider slider;
+    private Color iconColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        iconColor = audioIcon[0].color;
     }
 
     // Update is called once per frame
     void Update()
     {
+        var currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == "Cutscene")
+        {
+            audioIcon[0].color = Color.clear;
+            audioIcon[1].color = Color.clear;
+            slider.gameObject.SetActive(false);
+        }
+        else
+        {
+            audioIcon[0].color = iconColor;
+            audioIcon[1].color = iconColor;
+            slider.gameObject.SetActive(true);
+        }
+
         if (slider.value > 0)
         {
             audioIcon[0].enabled = true;
