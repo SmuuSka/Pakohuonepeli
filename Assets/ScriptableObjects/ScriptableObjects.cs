58dff56298d32da81b6fb5e01f0aaa99b502e7d9
Mashadeve <maenpaa.nk@gmail.com>
2022-04-07 12:29:12 +0300

    ScriptableObjects

---
+++ Assets/ScriptableObjects/ScriptableObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "playerData", menuName = "playerMenu")]
+public class ScriptableObjects : ScriptableObject
+{
+    public string playerName;
+    public List<string> nameList = new List<string>();
+    
+
+}
---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public ScriptableObjects so;
+    public InputField nameInput;
+
+    public void EnterNameButton()
+    {
+        var nameInputField = nameInput.text;
+        so.nameList.Add(nameInputField);
+    }
+
+
+
+}
