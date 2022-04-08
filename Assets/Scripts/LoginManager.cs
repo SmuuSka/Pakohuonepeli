5f6deb6ae0ccf8631bdd3c71fb59a2efdaeca738
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2022-04-08 10:15:14 +0300

    database listan rakentelua

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -7,13 +7,34 @@ public class LoginManager : MonoBehaviour
 {
     public ScriptableObjects so;
     public InputField nameInput;
+    private bool canBeAdded = false;
+
+
 
     public void EnterNameButton()
     {
-        var nameInputField = nameInput.text;
-        so.nameList.Add(nameInputField);
+        var userNameInput = nameInput.text;
+
+        if (so.nameList.Count < 1)
+        {
+            so.nameList.Add(userNameInput);
+        }
+        else if (canBeAdded == false)
+        {
+            foreach (var name in so.nameList)
+            {
+                if (name == userNameInput)
+                {
+                    Debug.Log("Käyttäjä on olemassa");
+                    break;
+                }
+                else
+                {
+                    Debug.Log("Käyttäjä on luotu");
+                    so.nameList.Add(userNameInput);
+                    canBeAdded = true;
+                }
+            }
+        }                 
     }
-
-
-
 }
